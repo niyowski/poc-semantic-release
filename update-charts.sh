aefbeca6ff0c2ebbabbc2e10eac3ec361565f786
Mehmet Yarar <niyowski@gmail.com>
2024-02-16 19:00:57 +0600

    chore: set up semantic release

---
+++ update-charts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script is used to update the image tag in the values.yaml file of a Helm chart.
+# It takes a single argument, which is the new version of the image.
+# Usage: ./update-charts.sh <new-version>
+
+# The new version is passed as an argument to the script
+NEW_VERSION=$1
+
+# The file to update
+FILE="charts/values.yaml"
+
+# Use sed to replace the image/tag value
+sed -i '' "s|image/tag: .*|image/tag: $NEW_VERSION|" $FILE
+
