7805b84c2481f268d7c826dffb6b932bac1ad7f3
Mehmet Yarar <niyowski@gmail.com>
2024-02-19 05:08:52 +0600

    feat: add release rules

--- release.config.js
+++ release.config.js
@@ -5,7 +5,27 @@ module.exports = {
   branches: ["develop"],
   plugins: [
     // https://github.com/semantic-release/commit-analyzer
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "docs", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "test", release: "patch" },
+          { type: "build", release: "patch" },
+          { type: "ci", release: "patch" },
+          { type: "chore", release: "patch" },
+        ],
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+        },
+      },
+    ],
 
     // https://github.com/semantic-release/release-notes-generator
     "@semantic-release/release-notes-generator",
